408aba522a10fb3a128f19bce17257f110dfd4fc
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-29 17:30:09 +0530

    Added some more .env and added config.ts to access envs in simple way

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,9 @@
+export const appWriteConfig = {
+  endpointUrl: process.env.NEXTPUBLICAPPWRITEENDPOINT!,
+  projectId: process.env.NEXTPUBLICAPPWRITEPROJECT!,
+  databaseId: process.env.NEXTPUBLICAPPWRITEDATABASE!,
+  usersCollectionId: process.env.NEXTPUBLICAPPWRITEUSERSCOLLECTION!,
+  filesCollectionId: process.env.NEXTPUBLICAPPWRITEFILESCOLLECTION!,
+  bucketId: process.env.NEXTPUBLICAPPWRITEBUCKET!,
+  secretKey: process.env.NEXTAPPWRITESECRET!,
+};
