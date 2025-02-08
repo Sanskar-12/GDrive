8070777250d6b6ba3ee77d0e42bfd929c7937eb6
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-08 11:59:21 +0530

    making of actions dropdown

---
+++ components/ActionsDropdown.tsx
@@ -0,0 +1,34 @@
+import { Models } from "node-appwrite";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Dialog } from "./ui/dialog";
+
+interface ActionsDropdownProps {
+  file: Models.Document;
+}
+
+const ActionsDropdown = ({ file }: ActionsDropdownProps) => {
+  return (
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  );
+};
+
+export default ActionsDropdown;
