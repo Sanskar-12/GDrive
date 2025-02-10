3656e7efd84b3d0ee35705055fc79e8b3eef8217
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-10 12:00:35 +0530

    Rename File logic integrated

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -24,12 +24,16 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { renameFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 interface ActionsDropdownProps {
   file: Models.Document;
 }
 
 const ActionsDropdown = ({ file }: ActionsDropdownProps) => {
+  const path = usePathname();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [action, setAction] = useState<ActionType | null>(null);
@@ -43,7 +47,24 @@ const ActionsDropdown = ({ file }: ActionsDropdownProps) => {
     setAction(null);
   };
 
-  const handleAction = () => {};
+  const handleAction = async () => {
+    if (!action) return;
+    setIsLoading(true);
+    let success = false;
+
+    const actions = {
+      rename: () =>
+        renameFile({ fileId: file.$id, name, extension: file.extension, path }),
+      share: () => console.log("share"),
+      delete: () => console.log("delete"),
+    };
+
+    success = await actions[action.value as keyof typeof actions]();
+
+    if (success) handleCloseModals();
+
+    setIsLoading(false);
+  };
 
   const renderDialogContent = () => {
     if (!action) return null;
