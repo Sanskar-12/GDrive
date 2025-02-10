8d0d229eb9f80df2ae71f0d51c6d31b24477ce52
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-10 11:49:28 +0530

    Rename file server action done

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -98,3 +98,31 @@ export const getFiles = async () => {
     handleError(error, "Failed to get files");
   }
 };
+
+export const renameFile = async ({
+  fileId,
+  name,
+  extension,
+  path,
+}: RenameFileProps) => {
+  try {
+    const { databases } = await createAdminClient();
+
+    const newName = `${name}.${extension}`;
+
+    const updatedFile = await databases.updateDocument(
+      appWriteConfig.databaseId,
+      appWriteConfig.filesCollectionId,
+      fileId,
+      {
+        name: newName,
+      }
+    );
+
+    revalidatePath(path);
+
+    return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, "Cannot rename the file");
+  }
+};
