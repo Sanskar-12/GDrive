7ba36f14277857af667b38ba7ade13ff63074c38
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-11 13:07:09 +0530

    delete files functionality done

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -23,7 +23,11 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
+import {
+  deleteFile,
+  renameFile,
+  updateFileUsers,
+} from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
 import { FileDetails } from "./ActionModalContent";
 import ShareInput from "./ShareInput";
@@ -71,7 +75,8 @@ const ActionsDropdown = ({ file }: ActionsDropdownProps) => {
       rename: () =>
         renameFile({ fileId: file.$id, name, extension: file.extension, path }),
       share: () => updateFileUsers({ fileId: file.$id, emails, path }),
-      delete: () => console.log("delete"),
+      delete: () =>
+        deleteFile({ fileId: file.$id, bucketFileId: file.bucketFileId, path }),
     };
 
     success = await actions[action.value as keyof typeof actions]();
