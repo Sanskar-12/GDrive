de30562b8d6f21fb2e7a0182554c3197a925209d
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-12 10:36:20 +0530

    filter with types functionality done

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,13 +1,15 @@
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
-import { convertFileSize } from "@/lib/utils";
+import { convertFileSize, getFileTypesParams } from "@/lib/utils";
 import { Models } from "node-appwrite";
 
 const Page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
 
-  const files = await getFiles();
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types });
 
   let tot = 0;
 
