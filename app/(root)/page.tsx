649fb5e7475f95ab75d768f9951eac82cb1cadae
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-14 18:28:00 +0530

    Total Space Used Chart implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
+import Chart from "@/components/Chart";
 import { getTotalSpaceUsed } from "@/lib/actions/file.actions";
 
 export default async function Home() {
   const totalSpaceUsed = await getTotalSpaceUsed();
 
-  console.log(totalSpaceUsed);
-
   return (
-    <div className="flex-center h-screen">
-      <div className="h1">StoreIt</div>
+    <div className="dashboard-container">
+      {/* Charts and Files Summary */}
+      <section>
+        <Chart used={totalSpaceUsed.used} />
+      </section>
+
+      {/* Recent Files Uploaded */}
+      <section></section>
     </div>
   );
 }
