4313117a397cdc457f287054b83c90763d070da2
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-15 13:10:32 +0530

    css changed

--- components/ActionModalContent.tsx
+++ components/ActionModalContent.tsx
@@ -12,7 +12,7 @@ export const ImageThumbnail = ({ file }: { file: Models.Document }) => {
     <div className="file-details-thumbnail">
       <Thumbnail type={file.type} extension={file.extension} url={file.url} />
       <div className="flex flex-col">
-        <p className="subtitle-2 mb-1">{file.name}</p>
+        <p className="subtitle-2 mb-1 line-clamp-1">{file.name}</p>
         <FormattedDateTime date={file.$createdAt} className="caption" />
       </div>
     </div>
